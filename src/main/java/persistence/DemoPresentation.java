6e3f5cec482c13acb6becd30e4f37fe4ace23ba7
Pharrellstu <pharrell.buckman@student.nhlstenden.com>
2026-03-31 11:34:46 +0200

    Replace image format in demo presentation from JPG to GIF.

--- src/main/java/persistence/DemoPresentation.java
+++ src/main/java/persistence/DemoPresentation.java
@@ -44,7 +44,7 @@ public class DemoPresentation implements Accesor {
         slide.append(2, "use File->Open from the menu.");
         slide.append(1, " ");
         slide.append(1, "This is the end of the presentation.");
-        slide.append(new BitmapItem(1, "JabberPoint.jpg"));
+        slide.append(new BitmapItem(1, "JabberPoint.gif"));
         presentation.append(slide);
     }
 
